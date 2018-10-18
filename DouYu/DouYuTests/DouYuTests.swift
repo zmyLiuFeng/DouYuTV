4c798342f3823f3ec09d2bc07ff2d2faea63afa1
朱满营 <zmy@zmy.local>
2018-10-18 15:10:44 +0800

    --第一次提价

---
+++ DouYu/DouYuTests/DouYuTests.swift
@@ -0,0 +1,34 @@
+//
+//  DouYuTests.swift
+//  DouYuTests
+//
+//  Created by 朱满营 on 2018/10/18.
+//  Copyright © 2018年 朱满营. All rights reserved.
+//
+
+import XCTest
+@testable import DouYu
+
+class DouYuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
